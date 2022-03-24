6054276a96d3197279696f5572c0215d445d82db
kaydeedalegend <mixtakaydee@gmail.com>
2022-03-24 14:13:48 +0100

    Updated port to accept possible enviroment port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import userRoutes from "./routes/users.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use("/users", userRoutes);
